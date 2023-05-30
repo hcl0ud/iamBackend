1410d1743feb277052b844f85792a8f1019161f2
cl0ud <148cl0ud@gmail.com>
2023-05-30 14:46:32 +0900

    writeBoard 수정

--- src/models/board.js
+++ src/models/board.js
@@ -1,8 +1,8 @@
 const { db } = require("./index");
 const dayjs = require("dayjs");
-const commonUtil = require("../util/commonUtil.js");
 
 const board = db.collection("board");
+const user = db.collection("user");
 
 exports.getBoardData = async (ctx) => {
   const data = await board.find({}, {}).toArray();
@@ -25,15 +25,18 @@ exports.writeBoard = async (ctx) => {
   let now = dayjs();
   let time = now.format().slice(0, 19).split("T").join(" ");
 
-  if (commonUtil.checkForm(ctx.request.body)) {
+  if (ctx.request.body) {
     const { boardTitle, boardContents, userName } = ctx.request.body;
+    const userIdx = await user.findOne({
+      userEmail: userName.userIdx,
+    });
 
     await board
       .insertOne({
         writeTime: time,
         boardTitle: boardTitle,
         boardContents: boardContents,
-        userName: userName,
+        userName: userIdx.userName,
         likeCount: 0,
       })
       .then((ctx.body = { status: 200, resultCode: 1 }))
@@ -44,3 +47,5 @@ exports.writeBoard = async (ctx) => {
     ctx.body = { status: 200, resultCode: 0, error: "include null data" };
   }
 };
+
+exports.getBoardDetail = async (ctx) => {};
