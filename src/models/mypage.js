9ddfd5688a4a32f7af60a8d2113afa1691b19f2d
Seungki kim <ti9918@gmail.com>
2023-06-08 14:24:15 +0900

    mypage 수정

--- src/models/mypage.js
+++ src/models/mypage.js
@@ -13,10 +13,10 @@ router.post("/upload-profile-photo", upload.single("photo"), async (ctx) => {
   // 파일 업로드 성공 시, 파일 경로를 DB에 저장합니다.
   if (file) {
     const photoPath = file.path;
-    await user.updateOne({ userIdx: userIdx }, { $set: { photoPath: photoPath } });
+    await user.findeOne({ userIdx: userIdx }, { $set: { photoPath: photoPath } });
     ctx.body = { status: 200, resultCode: 1, message: "프로필 사진이 업로드되었습니다." };
   } else {
-    ctx.body = { status: 400, resultCode: 0, error: "파일 업로드에 실패했습니다." };
+    ctx.body = { status: 200, resultCode: 0, error: "파일 업로드에 실패했습니다." };
   }
 });
 
@@ -24,6 +24,6 @@ router.post("/update-introduction", async (ctx) => {
   const userIdx = ctx.state.userIdx;
   const { introduction } = ctx.request.body;
 
-  await user.updateOne({ userIdx: userIdx }, { $set: { introduction: introduction } });
+  await user.findOne({ userIdx: userIdx }, { $set: { introduction: introduction } });
   ctx.body = { status: 200, resultCode: 1, message: "소개말이 업데이트되었습니다." };
 });
