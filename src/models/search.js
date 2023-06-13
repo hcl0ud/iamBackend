e3cd0245ab4b4e4a7506ab563bcc80f2909e3798
GithubWoon <TW@DESKTOP-0MQHC5R>
2023-06-13 14:37:57 +0900

    taewoon.search

--- src/models/search.js
+++ src/models/search.js
@@ -7,18 +7,36 @@ board.createIndex({ boardTitle: "text", boardcontents: "text" });
 exports.search = async (ctx) => {
   const { keyword } = ctx.request.body;
 
-  const data = await board.find({ $text: { $search: keyword } }, {}).toArray();
-  if (data) {
-    ctx.body = {
-      status: 200,
-      resultCode: 1,
-      data: data.reverse(),
-    };
+  const indexes = await board.getIndexes();
+  const textIndex = indexes.find((index) => {
+    return (
+      index.hasOwnProperty("key") &&
+      index.key.hasOwnProperty("boardTitle") &&
+      index.key.hasOwnProperty("boardcontents")
+    );
+  });
+
+  if (textIndex) {
+    const data = await board.find({ $text: { $search: keyword } }).toArray();
+
+    if (data.length > 0) {
+      ctx.body = {
+        status: 200,
+        resultCode: 1,
+        data: data.reverse(),
+      };
+    } else {
+      ctx.body = {
+        status: 200,
+        resultCode: 0,
+        error: "데이터 조회 실패",
+      };
+    }
   } else {
     ctx.body = {
       status: 200,
       resultCode: 0,
-      error: "데이터 조회 실패",
+      error: "텍스트 인덱스가 존재하지 않습니다.",
     };
   }
 };
