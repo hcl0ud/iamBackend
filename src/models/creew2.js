ebfc8627de394db0a96cf9d25547f3dc9a557cc1
GithubWoon <TW@DESKTOP-0MQHC5R>
2023-06-18 17:17:29 +0900

    taewoon

--- src/models/creew2.js
+++ src/models/creew2.js
@@ -1,39 +1,49 @@
 const { db } = require("../models");
 const user = db.collection("user");
-const board = db.collection("crew");
+const crewuser = db.collection("crewuser");
 
-app.use(express.static('public'));
+exports.joinCrew = async (ctx) => { 
 
-// 추천 크루 가져오는 API 엔드포인트
-app.get('/crew/get-recommended-crew', async (ctx) => {
-  // 예시 데이터로 대체
-  const recommendedCrews = [
-    {
-      _id: '크루1_ID',
-      name: '추천 크루 1',
-      introduction: '추천 크루 1 소개',
-      memberCount: 10,
-      postCount: 20,
-    },
-    {
-      _id: '크루2_ID',
-      name: '추천 크루 2',
-      introduction: '추천 크루 2 소개',
-      memberCount: 15,
-      postCount: 30,
-    },
-    //...
-  ];
+  let now = dayjs();
+  let time = now.format().slice(0, 19).split("T").join(" ");
 
-  ctx.body = { recommendedCrews };
-});
+  if (ctx.request.body) {
 
-// 크루 가입 API 엔드포인트
-app.post('/crew/join-crew', async (ctx) => {
-  const { crewId } = ctx.request.body;
+    const { crewId } = ctx.request.body;
+    const userInfo = await user.findOne({ userEmail: userIdx });
 
-  // 크루 가입 로직 작성
-  // crewId를 기반으로 크루를 조회하고, 사용자를 크루에 추가하는 로직을 구현
+    await crewuser
+      .insertOne({
+        crewId: crewId,
+        userName: userInfo.userName,
+      })
+      .then((ctx.body = { status: 200, resultCode: 1 }))
+      .catch((e) => {
+        ctx.body = { status: 200, resultCode: 0, error: e };
+      });
+  } else {
+    ctx.body = { status: 200, resultCode: 0, error: "include null data" };
+  }
 
-  ctx.body = '크루 가입이 완료되었습니다.';
-});
+}
+
+exports.getCrewList = async (ctx) => {
+
+  const data = await crew.find({}, {}).toArray();
+  try {
+    ctx.body = {
+      status: 200,
+      resultCode: 1,
+      data: data.reverse(),
+    };
+  } catch (e) {
+    ctx.body = {
+      status: 200,
+      resultCode: 0,
+      error: "데이터 조회 실패",
+      msg: e,
+    };
+  }
+
+}
+
--- src/models/crew1.js
+++ src/models/crew1.js
@@ -1,132 +1,115 @@
-// routes/index.js
+const { db } = require(".")
+const user = db.collection("user");
+const crew = db.collection("crew");
 
-const router = require('koa-router')();
-const { User, Crew, Post } = require('../models');
+// 게시물 모델 정의
+
+  title: String,
+  content: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+
+// 크루 모델 정의
+
+  name: String,
+  introduction: String,
+  profileImage: String,
+  
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // 크루 생성 API 엔드포인트
-router.post('/crew/create-crew', async (ctx) => {
-  const { name, introduction, profileImage } = ctx.request.body;
+app.router('/crew/create-crew', (req, res) => {
+  const { name, introduction, profileImage } = req.body;
 
-  try {
-    // 중복된 크루명인지 확인
-    const existingCrew = await Crew.findOne({ name: name });
+  // 중복된 크루명인지 확인
+  Crew.findOne({ name: name })
+    .then((existingCrew) => {
+      if (existingCrew) {
+        return res.status(409).send('이미 존재하는 크루명입니다.');
+      }
 
-    if (existingCrew) {
-      ctx.status = 409;
-      ctx.body = '이미 존재하는 크루명입니다.';
-      return;
-    }
+      // 새로운 크루 생성
+      const newCrew = new Crew({
+        name,
+        introduction,
+        profileImage,
+      });
 
-    // 새로운 크루 생성
-    const newCrew = new Crew({
-      name,
-      introduction,
-      profileImage,
+      // 크루 저장
+      newCrew
+        .save()
+        .then(() => {
+          res.status(201).send('크루가 성공적으로 생성되었습니다.');
+        })
+        .catch((err) => {
+          console.error('크루 생성 오류:', err);
+          res.status(500).send('크루 생성 중 오류가 발생했습니다.');
+        });
+    })
+    .catch((err) => {
+      console.error('크루 조회 오류:', err);
+      res.status(500).send('크루 생성 중 오류가 발생했습니다.');
     });
-
-    // 크루 저장
-    await newCrew.save();
-
-    ctx.status = 201;
-    ctx.body = '크루가 성공적으로 생성되었습니다.';
-  } catch (err) {
-    console.error('크루 생성 오류:', err);
-    ctx.status = 500;
-    ctx.body = '크루 생성 중 오류가 발생했습니다.';
-  }
 });
 
 // 게시물 조회 API 엔드포인트
-router.get('/crew/posts/:crewId', async (ctx) => {
-  const { crewId } = ctx.params;
+app.get('/crew/posts/:crewId', (req, res) => {
+  const { crewId } = req.params;
 
-  try {
-    // 크루에 속한 게시물 조회
-    const crew = await Crew.findById(crewId).populate({
-      path: 'posts',
-      populate: {
-        path: 'author',
-        model: 'User',
-      },
-    }).sort({ 'posts.createdAt': -1 });
+  // 크루에 속한 게시물 조회
+  Crew.findById(crewId)
+    .populate('posts')
+    .sort({ 'posts.createdAt': -1 })
+    .exec((err, crew) => {
+      if (err) {
+        console.error('게시물 조회 오류:', err);
+        return res.status(500).send('게시물 조회 중 오류가 발생했습니다.');
+      }
 
-    if (!crew) {
-      ctx.status = 404;
-      ctx.body = '크루를 찾을 수 없습니다.';
-      return;
-    }
+      if (!crew) {
+        return res.status(404).send('크루를 찾을 수 없습니다.');
+      }
 
-    // 현재 사용자의 크루 소속 여부 확인
-    // 예시로 사용자 ID를 'userId'라고 가정합니다.
-    const userId = 'userId';
-
-    if (crew.members.includes(userId)) {
-      // 현재 사용자가 크루에 속한 경우 게시물 목록 반환
-      ctx.body = crew.posts;
-    } else {
-      // 현재 사용자가 크루에 속하지 않은 경우 권한 없음 에러 반환
-      ctx.status = 403;
-      ctx.body = '해당 크루에 접근할 권한이 없습니다.';
-    }
-  } catch (err) {
-    console.error('게시물 조회 오류:', err);
-    ctx.status = 500;
-    ctx.body = '게시물 조회 중 오류가 발생했습니다.';
-  }
+      res.send(crew.posts);
+    });
 });
 
 // 게시물 생성 API 엔드포인트
-router.post('/crew/create-post', async (ctx) => {
-  const { crewId, title, content } = ctx.request.body;
+app.router('/crew/create-post', (req, res) => {
+  const { crewId, title, content } = req.body;
 
-  // 현재 사용자의 ID
-  // 예시로 사용자 ID를 'userId'라고 가정합니다.
-  const userId = 'userId';
+  // 크루에 속한 게시물 생성
+  const newPost = new Post({
+    title,
+    content,
+  });
 
-  try {
-    // 사용자가 속한 크루인지 확인
-    const user = await User.findById(userId);
-
-    if (!user) {
-      ctx.status = 404;
-      ctx.body = '사용자를 찾을 수 없습니다.';
-      return;
+  newPost.save((err, post) => {
+    if (err) {
+      console.error('게시물 생성 오류:', err);
+      return res.status(500).send('게시물 생성 중 오류가 발생했습니다.');
     }
 
-    // 사용자가 속한 크루 목록에서 입력된 크루 ID를 찾음
-    if (user.crews.includes(crewId)) {
-      // 크루에 속한 게시물 생성
-      const newPost = new Post({
-        title,
-        content,
-        author: userId,
-      });
+    Crew.findByIdAndUpdate(
+      crewId,
+      { $push: { posts: post._id } },
+      { new: true },
+      (err, crew) => {
+        if (err) {
+          console.error('게시물 생성 오류:', err);
+          return res.status(500).send('게시물 생성 중 오류가 발생했습니다.');
+        }
 
-      await newPost.save();
+        if (!crew) {
+          return res.status(404).send('크루를 찾을 수 없습니다.');
+        }
 
-      const crew = await Crew.findByIdAndUpdate(
-        crewId,
-        { $push: { posts: newPost._id } },
-        { new: true }
-      );
-
-      if (!crew) {
-        ctx.status = 404;
-        ctx.body = '크루를 찾을 수 없습니다.';
-        return;
+        res.status(201).send('게시물이 성공적으로 생성되었습니다.');
       }
-
-      ctx.status = 201;
-      ctx.body = '게시물이 성공적으로 생성되었습니다.';
-    } else {
-      ctx.status = 403;
-      ctx.body = '게시물을 작성할 권한이 없습니다.';
-    }
-  } catch (err) {
-    console.error('게시물 생성 오류:', err);
-    ctx.status = 500;
-    ctx.body = '게시물 생성 중 오류가 발생했습니다.';
-  }
+    );
+  });
 });
 
-module.exports = router;
