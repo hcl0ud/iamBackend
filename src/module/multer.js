55ee5c1fd02b1d90895119d87426660342a4d0c0
cl0ud <148cl0ud@gmail.com>
2023-06-20 13:46:46 +0900

    파일 업로드 로직 수정6

--- src/module/multer.js
+++ src/module/multer.js
@@ -11,7 +11,7 @@ const upload = multer({
           if (e) {
             fs.mkdirSync(path.resolve("uploads/profile/", req.body.userIdx));
           }
-          cb(null, "uploads/profile/" + req.body.userIdx);
+          cb(null, path.resolve("uploads/profile/", req.body.userIdx));
         }
       );
     },
