d4ece209786ad20b508526124187ffac613bd00f
GithubWoon <TW@DESKTOP-0MQHC5R>
2023-06-22 17:03:39 +0900

    taewoon

--- src/models/createcrew.js
+++ src/models/createcrew.js
@@ -8,13 +8,7 @@ const crewuser = db.collection("crewuser");
 exports.createCrew = async (ctx) => { 
 
   if (ctx.request.body) {
-    const maxCrew = await crew.find({ crewId: crewId }, {}).toArray();
-    const currentCrewCount = maxCrew.length;
-    const newCrewId = currentCrewCount + 1;
-    const { crewName, profileimag, introduction, } = ctx.request.body;
-    const userInfo = await user.findOne({ userEmail: userIdx });
-    const userIdx = 123; // 사용할 userIdx 값
-    const url = `http://localhost:5173/CrewMain?userIdx=${userIdx}`;
+    
 
     // 1. 크루 생성
     await crew
