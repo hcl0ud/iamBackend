63b21eebf69137f1ac9b401548a7d025476ae2f8
GithubWoon <TW@DESKTOP-0MQHC5R>
2023-06-23 00:45:34 +0900

    createcrew완성

--- src/models/crew.js
+++ src/models/crew.js
@@ -4,36 +4,27 @@ const crew = db.collection("crew");
 const board = db.collection("board");
 
 // 크루생성
-  exports.createCrew = async (ctx) => { 
-
-    const { crewName, profileimag, introduction, userIdx} = ctx.request.body;
-    //이러면 UserName 이 중복된 사람들끼리일땐 어떻게 하지
-    const userInfo = await user.findOne({ userName: userIdx });
-    const UserName = db.collection("user");
-
-    // 추가되는 크루정보
-
-    await crew {
-      .insertOne({
-        crewId: newCrewId,
-        crewName: crewName,
-        profileimag: profileimg,
-        introduction: introduction,
-        ownerName: userInfo.userName,
-      })
-    };
+exports.createCrew = async (ctx) => {
+  const { crewName, profileImg, crewIntro, userIdx } = ctx.request.body;
+  const userInfo = await user.findOne({ userEmail: userIdx });
 
   // 동일한 크루명이 있는지 확인
-  try {
-    const SamecrewName = await crew.findeOne ({CrewName});
-    if (SamecrewName) {
-      ctx.body = {status: 200, resultCode: 1, error: "중복된 크루명입니다"};
-      return;
-    }
+  const SamecrewName = await crew.findOne({ crewName: crewName });
+  if (SamecrewName) {
+    ctx.body = { status: 200, resultCode: 0, error: "중복된 크루명입니다" };
+  } else {
+    await crew.insertOne({
+      crewName: crewName,
+      profileImg: profileImg,
+      crewIntro: crewIntro,
+      ownerName: userInfo.userName,
+    });
+    ctx.body = { status: 200, resultCode: 1 };
+  }
+};
 
 // 크루 가입
 exports.JoinCrew = async (ctx) => {
-  const crewName = request.body
-  const crew
-
-}
+  const crewName = ctx.request.body;
+  // const crew
+};
