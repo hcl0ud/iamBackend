86dbdccacaa64ec34f52372595418b1c9c8f7324
cl0ud <148cl0ud@gmail.com>
2023-07-06 20:52:09 +0900

    프론트 배포

--- src/index.js
+++ src/index.js
@@ -11,15 +11,15 @@ const mount = require("koa-mount");
 
 const app = new Koa();
 const dirPath = path.resolve("./uploads");
+const front = path.resolve("./public");
 
 app
   .use(cors())
   .use(parser({ multipart: true, urlencoded: true }))
   .use(router.routes())
   .use(router.allowedMethods())
+  .use(mount("/", serve(front)))
   .use(mount("/uploads", serve(dirPath)))
   .listen(API_PORT, () => {
     console.log(`Connected to http://${API_IP}:${API_PORT}`);
   });
-
-  
